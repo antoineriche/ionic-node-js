1751df70f82ce3da7da0f142d3d48a4cf3cf6ff7
antoineriche <riche.antoine.pro@gmail.com>
2018-11-20 15:21:42 +0100

    update server to make request to openbordeaux

--- server/my-server.js
+++ server/my-server.js
@@ -2,8 +2,10 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 var https = require('https');
-var http = require('http').Server(app);
+var http = require('http');
+var server = http.Server(app);
 var bodyParser = require('body-parser');
+var request = require('request');
 
 
 console.log('start my-server');
@@ -15,17 +17,35 @@ app.options('*', cors());
 app.get('/', function(req, res) {
 	var ip = req.connection.remoteAddress;
 	console.log(ip + ' is getting');
-
-	https.get('https://jsonplaceholder.typicode.com/todos', (resp) => {
+	
+	var url = "http://odata.bordeaux.fr/v1/databordeaux/poisport/?format=json";
+	
+	http.get(url, (resp) => {
 		let data = '';
+		let jsonResp = {};
 
 	  	resp
 	  		.on('data', (chunk) => data += chunk)
-			.on('end', () => res.json(JSON.parse(data)))
-	  		.on("error", (err) => console.log("Error: " + err.message));
+			.on('end', () => {
+				try {
+					jsonResp.data = JSON.parse(data);
+					jsonResp.success = true;
+				} catch(error){
+					jsonResp.success = false;
+					jsonResp.error = { message: error.message };
+				} finally {
+					res.json(jsonResp);
+				}
+			})
+	  		.on("error", (err) => {
+	  			console.log("Error: " + err.message);	
+	  			jsonResp.success = false;
+	  			jsonResp.error = { message: err.message };
+	  			res.json(jsonResp)
+	  		});
 	});
 });
 
-http.listen(8080, function(){
-    console.log('listening on: 8080');
+server.listen(8080, function(){
+    console.log('listening on: 8090');
 });
