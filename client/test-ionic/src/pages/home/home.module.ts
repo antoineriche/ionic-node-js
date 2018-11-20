82fc1587355701d8771898be95dbda6b37d3e7d8
antoineriche <riche.antoine.pro@gmail.com>
2018-11-20 19:50:45 +0100

    fix multi-import from client home module

--- client/test-ionic/src/pages/home/home.module.ts
+++ client/test-ionic/src/pages/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
