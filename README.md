4a2d75b49a07dff67574df48253c6a718d4eb57e
antoineriche <riche.antoine.pro@gmail.com>
2018-11-20 19:38:27 +0100

    change buttons client label

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # ionic-node-js
 Cross-platform application and Node.js server
-
-## Installation
-
-### MongoDB
-Download and install MongoDb.<br/>
--- client/test-ionic/src/pages/home/home.module.ts
+++ client/test-ionic/src/pages/home/home.module.ts
@@ -10,7 +10,7 @@ import { HomePage } from './home.page';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule, ReactiveFormsModule
+    FormsModule, ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild([
       {
