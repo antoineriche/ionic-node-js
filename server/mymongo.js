5dde33cf28ec38a7888f4e85f9d2712385929586
antoineriche <riche.antoine.pro@gmail.com>
2018-11-20 21:33:50 +0100

    rewrite server

--- server/mymongo.js
+++ server/mymongo.js
@@ -1,88 +1,64 @@
 var MongoClient = require('mongodb').MongoClient;
 
 const DB_NAME = "mydb";
-const SPORTPOINTS_COLLECTION = "sportpoints";
+const POSTS_COLLECTION = "posts";
 const MONGO_URL = "mongodb://localhost:27017/" + DB_NAME;
 
-var connect = function(callback){
+
+var connect = function(rootCallback, childCallback){
   return MongoClient.connect(MONGO_URL, { useNewUrlParser: true }, 
     function(err, db){
-      callback(err, db);
+      let jsonResp = {};
+      if (err) {
+        console.log(err);
+        jsonResp.success = false;
+        jsonResp.error = { message: err.message };
+        rootCallback(jsonResp);
+      } else {
+        childCallback(db, function(err, data){
+          if(!err){
+            jsonResp.success = true;
+            jsonResp.data = data;
+          } else {
+              console.log(err);
+              jsonResp.success = true;
+              jsonResp.error = { message: err.message };
+          }
+          rootCallback(jsonResp);
+        });
+      }
       db.close();
     });
 }
 
 var getAllPosts = function(callback){
-  connect(function(err, db){
-    let jsonResp = {};
-    if (err) {
-      console.log(err);
-      jsonResp.success = false;
-      jsonResp.error = { message: err.message };
-      callback(jsonResp);
-    } else {
-        db.db(DB_NAME).collection(SPORTPOINTS_COLLECTION).find().toArray(
-          function(err, favorites){
-            if(!err){
-              jsonResp.success = true;
-              jsonResp.data = favorites;
-            } else {
-                console.log(err);
-                jsonResp.success = true;
-                jsonResp.error = { message: err.message };
-            }
-            callback(jsonResp);
-        });
+  
+  connect(callback, function(db, next){
+    db.db(DB_NAME).collection(POSTS_COLLECTION).find().toArray(
+      function(err, points){
+        next(err, points);
       }
+    );
   });
 }
 
 var getUserPosts = function(userId, callback){
-  connect(function(err, db){
-    let jsonResp = {};
-    if (err) {
-      console.log(err);
-      jsonResp.success = false;
-      jsonResp.error = { message: err.message };
-      callback(jsonResp);
-    } else {
-        db.db(DB_NAME).collection(SPORTPOINTS_COLLECTION).find({ userId: userId })
-          .toArray(function(err, favorites){
-            if(!err){
-              jsonResp.success = true;
-              jsonResp.data = favorites;
-            } else {
-                console.log(err);
-                jsonResp.success = true;
-                jsonResp.error = { message: err.message };
-            }
-            callback(jsonResp);
-        });
-      }
+
+  connect(callback, function(db, next){
+    db.db(DB_NAME).collection(POSTS_COLLECTION).find({ userId: userId })
+      .toArray(function(err, favorites){
+        next(err, favorites);
+    });
   });
 }
 
 var postData = function(data, callback){
-  connect(function(err, db) {
-    let jsonResp = {};
-    if (err) {
-      console.log(err);
-      jsonResp.success = false;
-      jsonResp.error = { message: err.message };
-      callback(jsonResp);
-    } else {
-        db.db(DB_NAME).collection(SPORTPOINTS_COLLECTION).insertOne(data, function(err, resp) {
-          if(!err){
-              jsonResp.success = true;
-              jsonResp.data = true;
-            } else {
-                console.log(err);
-                jsonResp.success = true;
-                jsonResp.error = { message: err.message };
-            }
-            callback(jsonResp);
-        });
-    }
+  
+  connect(callback, function(db, next){
+    db.db(DB_NAME).collection(POSTS_COLLECTION)
+      .insertOne(data, function(err, resp){
+        next(err, true);
+    });
   });
 }
 
